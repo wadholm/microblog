51d4745b2990396c47762ab98b0a7a3a9c316ae5
Andreas Arnesson <Andreas_osby@hotmail.com>
2019-10-11 16:26:54 +0200

    added gunicorn logging for app

--- scripts/deploy-app/4.supervisor.sh
+++ scripts/deploy-app/4.supervisor.sh
@@ -1,3 +1,6 @@
 sudo cp scripts/deploy-app/resources/supervisor.conf /etc/supervisor/conf.d/microblog.conf
 
+sudo mkdir /var/log/microblog
+sudo chmod 755 /var/log/microblog
+
 sudo supervisorctl reload
