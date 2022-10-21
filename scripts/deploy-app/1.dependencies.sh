7f81842c49d025a952b8bb957371d98995ce0dc1
Andreas Arnesson <Andreas_osby@hotmail.com>
2022-10-21 14:54:32 +0200

    updated dependencies to validate and test code

--- scripts/deploy-app/1.dependencies.sh
+++ scripts/deploy-app/1.dependencies.sh
@@ -1,3 +1,3 @@
 sudo apt-get -y install python3-venv python3-dev
-sudo apt-get -y install supervisor nginx
+sudo apt-get -y install supervisor nginx make
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y default-mysql-server
