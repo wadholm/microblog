82b5a74533de92df5227e1fe1fb4ddc83e64b0bb
wadholm <malin.wadholm@gmail.com>
2022-11-08 16:54:36 +0100

    fix models validation

--- app/models.py
+++ app/models.py
@@ -49,23 +49,35 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def follow(self, user):
+        """
+        Add follower
+        """
         if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
+        """
+        Remove follower
+        """
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
+        """
+        Get followers
+        """
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-            followed = Post.query.join(
-                followers, (followers.c.followed_id == Post.user_id)).filter(
-                    followers.c.follower_id == self.id)
-            own = Post.query.filter_by(user_id=self.id)
-            return followed.union(own).order_by(Post.timestamp.desc())
+        """
+        Get followed posts
+        """
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
     @staticmethod
     @login.user_loader
